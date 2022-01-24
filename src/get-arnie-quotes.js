36be5f436f8e2d48486b85e22c7718379a7b849e
Pedram Marandi <pedram.marandi@gmail.com>
2022-01-24 13:13:03 +1100

    All tests passed

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,17 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const responses = await Promise.all(urls.map((url) => httpGet(url)));
+  return responses.map((res) => {
+    if (res.status === 200) {
+      return {
+        "Arnie Quote": JSON.parse(res.body).message,
+      };
+    }
+    return {
+      FAILURE: "Your request has been terminated",
+    };
+  });
 };
 
 module.exports = {
